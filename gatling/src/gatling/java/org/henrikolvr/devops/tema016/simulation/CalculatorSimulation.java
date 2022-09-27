ffc0b2cf334bc5bd1458bd9d2e29924361e0b01d
henrik-olvr <oliveira.hrkl@gmail.com>
2022-09-27 09:17:16 -0300

    testing

--- gatling/src/gatling/java/org/henrikolvr/devops/tema016/simulation/CalculatorSimulation.java
+++ gatling/src/gatling/java/org/henrikolvr/devops/tema016/simulation/CalculatorSimulation.java
@@ -13,7 +13,7 @@ public class CalculatorSimulation extends Simulation {
     int port = Integer.getInteger("port", 8080);
 
     HttpProtocolBuilder httpProtocol = http.baseUrl(protocol + "://" + host + ":" + port + "")
-            .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+            .acceptHeader("text/html,application/json,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
             .doNotTrackHeader("1")
             .acceptLanguageHeader("en-US,en;q=0.5")
             .acceptEncodingHeader("gzip, deflate")
